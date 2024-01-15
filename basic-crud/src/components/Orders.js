271e7ac2c2d8ada2854c032a4dd7e59f739c1f6c
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-16 00:29:38 +0100

    Update Orders.js
    Dummy Data ofc

--- basic-crud/src/components/Orders.js
+++ basic-crud/src/components/Orders.js
@@ -1,9 +1,18 @@
 import React from "react";
+import DataTable from "./DataTable";
 
 const Orders = () => {
+
+    const columns = ['Order No.', 'Customer ID', 'Del Status', 'Amount', 'Pay Protect', 'Refund Eligible'];
+  // Dummy data
+  const data = [
+    { 'Order No.': '123', 'Customer ID': 1, 'Del Status': 'Shipped', 'Amount': 50.00, 'Pay Protect': true, 'Refund Eligible': false },
+  ];
+
     return (
         <div>
             <h1>Orders</h1>
+            <DataTable columns={columns} data={data} />
         </div>
     )
 }
