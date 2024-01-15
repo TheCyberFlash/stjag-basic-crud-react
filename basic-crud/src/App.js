5e0c556e5833f09299c9f405a64acf52bac55970
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-16 00:15:56 +0100

    Update App.js
    App JS Setup, Rough plan of pages/routes

--- basic-crud/src/App.js
+++ basic-crud/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <Sidebar /> */}
+        <Routes>
+          {/* <Route exact path="/" component={Home} /> */}
+          {/* <Route exact path="/orders" component={Orders} /> */}
+          {/* <Route exact path="/customers" component={Customers} /> */}
+          {/* <Route exact path="/refunds" component={Refunds} /> */}
+          {/* <Route exact path="/config" component={Config} /> */}
+          {/* <Route component={NoMatch} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
