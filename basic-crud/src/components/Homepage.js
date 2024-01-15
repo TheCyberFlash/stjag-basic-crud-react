243bdf7707a5b0d66570d8f337a2b61c412072ca
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-16 00:50:05 +0100

    Update Homepage.js
    Buttons

--- basic-crud/src/components/Homepage.js
+++ basic-crud/src/components/Homepage.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
     return (
         <div>
             <h1>Homepage</h1>
+
+            <p>Welcome to the homepage.</p>
+
+            <p>Use the links in the sidebar to navigate to other pages.</p>
+
+            <p>Or, click the button below to go to the orders page.</p>
+            <Link to="/orders">
+                <button>Go to Orders</button>
+            </Link>
+
+            <p>Or, click the button below to go to the customers page.</p>
+            <Link to="/customers">
+                <button>Go to Customers</button>
+            </Link>
+
+            <p>Or, click the button below to go to the refunds page.</p>
+            <Link to="/refunds">
+                <button>Go to Refunds</button>
+            </Link>
         </div>
-    )
+    );
 }
 
 export default Homepage;
