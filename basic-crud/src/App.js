f32354974c944eeefcb91e868b9352bca0234ad2
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-16 00:27:29 +0100

    Quick create other components

--- basic-crud/src/App.js
+++ basic-crud/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Homepage from "./components/Homepage";
+import Orders from "./components/Orders";
+import Customers from "./components/Customers";
+import Refunds from "./components/Refunds";
+import Config from "./components/Config";
 
 function App() {
   return (
@@ -10,10 +14,10 @@ function App() {
         <Sidebar />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
-          {/* <Route exact path="/orders" element={<Orders />} /> */}
-          {/* <Route exact path="/customers" element={<Customers />} /> */}
-          {/* <Route exact path="/refunds" element={<Refunds />} /> */}
-          {/* <Route exact path="/config" element={<Config />} /> */}
+          <Route exact path="/orders" element={<Orders />} />
+          <Route exact path="/customers" element={<Customers />} />
+          <Route exact path="/refunds" element={<Refunds />} />
+          <Route exact path="/config" element={<Config />} />
           {/* <Route component={NoMatch} /> */}
         </Routes>
       </div>
---
+++ basic-crud/src/components/Refunds.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Refunds = () => {
+    return (
+        <div>
+            <h1>Refunds</h1>
+        </div>
+    )
+}
+
+export default Refunds;
