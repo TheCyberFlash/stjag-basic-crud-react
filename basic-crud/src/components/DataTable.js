0c75d3bc86af2f274c863711d7efa2c3a1711121
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-16 00:30:03 +0100

    Update DataTable.js
    Bug Fix, didn't display bool values

--- basic-crud/src/components/DataTable.js
+++ basic-crud/src/components/DataTable.js
@@ -1,27 +1,26 @@
-import React from "react";
+import React from 'react';
 
 const DataTable = ({ columns, data }) => {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    {columns.map((column, index) => (
-                        <th key={index}>{column}</th>
-                    ))}
-                </tr>
-            </thead>
-
-            <tbody>
-                {data.map((row, rowIndex) => (
-                <tr key={rowIndex}>
-                    {columns.map((column, colIndex) => (
-                    <td key={colIndex}>{row[column]}</td>
-                    ))}
-                </tr>
-                ))}
-            </tbody>
-        </table>
-    )
-}
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column, index) => (
+            <th key={index}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {columns.map((column, colIndex) => (
+              <td key={colIndex}>{row[column].toString()}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default DataTable;
