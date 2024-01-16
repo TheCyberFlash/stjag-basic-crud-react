6812bc5cb8a0e204cf13c503cd2df20b13be6cba
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-16 09:18:51 +0100

    Update Sidebar.js
    FontAwesomeIcon

--- basic-crud/src/components/Sidebar.js
+++ basic-crud/src/components/Sidebar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faShoppingCart, faUsers, faUndo, faCog } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <Link to="/">Home</Link>
-            <Link to="/orders">Orders</Link>
-            <Link to="/customers">Customers</Link>
-            <Link to="/refunds">Refunds</Link>
-            <Link to="/config">Config</Link>
+            <Link to="/"><FontAwesomeIcon icon={faHome} />{' '} Home</Link>
+            <Link to="/orders"><FontAwesomeIcon icon={faShoppingCart} />{' '} Orders</Link>
+            <Link to="/customers"><FontAwesomeIcon icon={faUsers} />{' '} Customers</Link>
+            <Link to="/refunds"><FontAwesomeIcon icon={faUndo} />{' '} Refunds</Link>
+            <Link to="/config"><FontAwesomeIcon icon={faCog} />{' '} Config</Link>
         </div>
     )
 }
