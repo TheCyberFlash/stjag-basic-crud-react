35ff887943fbc8535c3ea4962bce83eac6b1c597
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-18 00:55:10 +0100

    Update Filter.js
    Ensure that value is a scalar value

--- basic-crud/src/components/Filter.js
+++ basic-crud/src/components/Filter.js
@@ -4,9 +4,9 @@ const Filter = ({ label, value, options, onChange }) => {
     return (
         <div className="filter-container">
             <label>{label}</label>
-            <select value={value} onChange={(e) => onChange(e.target.value)}>
+            <select value={value.toString()} onChange={(e) => onChange(e.target.value)}>
                 {options.map((option, index) => (
-                    <option key={index} value={option.value}>
+                    <option key={index} value={option.value.toString()}>
                         {option.display}
                     </option>
                 ))}
