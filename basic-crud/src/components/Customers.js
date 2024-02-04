f398387e563054ff8d94a8f6450ca7959d0e75b4
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-04 20:08:52 +0100

    Update Customers.js

--- basic-crud/src/components/Customers.js
+++ basic-crud/src/components/Customers.js
@@ -2,57 +2,87 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import DataTable from "./DataTable";
 import Modal from "./Modal";
-import { selectFiltersData, addCustomer, updateFilters } from "../redux/customersSlice";
+import {
+  selectFiltersData,
+  addCustomer,
+  updateFilters,
+} from "../redux/customersSlice";
 
 const Customers = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [editedRecord, setEditedRecord] = useState(null);
-    const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editedRecord, setEditedRecord] = useState(null);
+  const dispatch = useDispatch();
 
-    const { data, selectedFilters, fields, filterDisplayNames, filters, columns } = useSelector((state) => state.customers);
-    const filteredData = useSelector(selectFiltersData);
+  const {
+    data,
+    selectedFilters,
+    fields,
+    filterDisplayNames,
+    filters,
+    columns,
+  } = useSelector((state) => state.customers);
+  const filteredData = useSelector(selectFiltersData);
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    }
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    }
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    const handleCreate = () => {
-        // alert('Create action');
-        openModal();
-    }
+  const handleCreate = () => {
+    // alert('Create action');
+    openModal();
+  };
 
-    const handleEdit = (record) => {
-        // alert(`Edit action for row with Customer ID.: ${record['Customer ID']}`);
-        openModal();
-        setEditedRecord(record);
-    }
+  const handleEdit = (record) => {
+    // alert(`Edit action for row with Customer ID.: ${record['Customer ID']}`);
+    openModal();
+    setEditedRecord(record);
+  };
 
-    const handleDelete = (record) => {
-        alert(`Delete action for row with Customer ID.: ${record['Customer ID']}`);
-    }
+  const handleDelete = (record) => {
+    alert(`Delete action for row with Customer ID.: ${record["Customer ID"]}`);
+  };
 
-    const handleSaveChanges = () => {
-        alert('Save Changes action');
-        closeModal();
-    }
+  const handleSaveChanges = () => {
+    alert("Save Changes action");
+    closeModal();
+  };
 
-    const handleFilterChange = (newFilters) => {
-        const { confirmed, activeOrder } = newFilters;
-        dispatch(updateFilters({ confirmedFilter: confirmed, activeOrdersFilter: activeOrder }));
-    }    
+  const handleFilterChange = (newFilters) => {
+    const { confirmed, activeOrder } = newFilters;
+    dispatch(
+      updateFilters({
+        confirmedFilter: confirmed,
+        activeOrdersFilter: activeOrder,
+      })
+    );
+  };
 
-    return (
-        <div>
-            <h1>Customers</h1>
-            <DataTable columns={columns} data={filteredData} handleCreate={handleCreate} handleEdit={handleEdit} 
-                handleDelete={handleDelete} filters={filters} filterDisplayNames={filterDisplayNames} handleFilterChange={handleFilterChange}/>
-            <Modal isOpen={isModalOpen} closeModal={closeModal} fields={fields} handleSaveChanges={handleSaveChanges} editedRecord={editedRecord}/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Customers</h1>
+      <DataTable
+        columns={columns}
+        data={filteredData}
+        handleCreate={handleCreate}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        filters={filters}
+        filterDisplayNames={filterDisplayNames}
+        handleFilterChange={handleFilterChange}
+      />
+      <Modal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        fields={fields}
+        handleSaveChanges={handleSaveChanges}
+        editedRecord={editedRecord}
+      />
+    </div>
+  );
+};
 
 export default Customers;
