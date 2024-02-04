c803a6508ce1c8c7f0f98cab31ac1c2f84c0a21d
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-04 20:09:40 +0100

    Update Modal.js

--- basic-crud/src/components/Modal.js
+++ basic-crud/src/components/Modal.js
@@ -1,50 +1,86 @@
 import React, { useEffect, useState } from "react";
 
-const Modal = ({ isOpen, closeModal, fields, handleSaveChanges, editedRecord }) => {
-    const [formValues, setFormValues] = useState({});
+const Modal = ({
+  isOpen,
+  closeModal,
+  fields,
+  handleSaveChanges,
+  editedRecord,
+}) => {
+  const [formValues, setFormValues] = useState({});
 
-    useEffect(() => {
-        if (editedRecord) {
-            const initialFormValues = {};
-            fields.forEach((field) => {
-                initialFormValues[field.label] = editedRecord[field.label];
-            });
-            setFormValues(initialFormValues);
-        }
-    }, [isOpen, editedRecord, fields]);
-
-    const handleFieldChange = (fieldName, value) => {
-        setFormValues((prevFormValues) => ({
-            ...prevFormValues,
-            [fieldName]: value,
-        }));
+  useEffect(() => {
+    if (editedRecord) {
+      const initialFormValues = {};
+      fields.forEach((field) => {
+        initialFormValues[field.label] = editedRecord[field.label];
+      });
+      setFormValues(initialFormValues);
     }
+  }, [isOpen, editedRecord, fields]);
 
-    const handleFormSave = () => {
-        handleSaveChanges(formValues);
-        closeModal();
-    }
+  const handleFieldChange = (fieldName, value) => {
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      [fieldName]: value,
+    }));
+  };
 
-    return (
-        isOpen && (
-            <div className="modal">
-                <div className="modal-component">
-                    <span className="modal-close" onClick={closeModal}>&times;</span>
+  const handleFormSave = () => {
+    handleSaveChanges(formValues);
+    closeModal();
+  };
 
-                    {fields.map((field, index) => (
-                        <div key={index}> 
-                            <label>{field.label}</label>
-                            {field.type === "text" && <input type="text" defaultValue={editedRecord ? editedRecord[field.label] : ''} onChange={(e) => handleFieldChange(field.label, e.target.value)}/>}
-                            {field.type === "number" && <input type="number" defaultValue={editedRecord ? editedRecord[field.label] : ''} onChange={(e) => handleFieldChange(field.label, e.target.value)}/>}
-                            {field.type === "boolean" && <input type="checkbox" defaultChecked={editedRecord ? editedRecord[field.label] : false} onChange={(e) => handleFieldChange(field.label, e.target.value)}/> }
-                        </div>
-                    ))}
+  return (
+    isOpen && (
+      <div className="modal">
+        <div className="modal-component">
+          <span className="modal-close" onClick={closeModal}>
+            &times;
+          </span>
 
-                    <button className="modal-save" onClick={handleFormSave}>Save Changes</button>
-                </div>
+          {fields.map((field, index) => (
+            <div key={index}>
+              <label>{field.label}</label>
+              {field.type === "text" && (
+                <input
+                  type="text"
+                  defaultValue={editedRecord ? editedRecord[field.label] : ""}
+                  onChange={(e) =>
+                    handleFieldChange(field.label, e.target.value)
+                  }
+                />
+              )}
+              {field.type === "number" && (
+                <input
+                  type="number"
+                  defaultValue={editedRecord ? editedRecord[field.label] : ""}
+                  onChange={(e) =>
+                    handleFieldChange(field.label, e.target.value)
+                  }
+                />
+              )}
+              {field.type === "boolean" && (
+                <input
+                  type="checkbox"
+                  defaultChecked={
+                    editedRecord ? editedRecord[field.label] : false
+                  }
+                  onChange={(e) =>
+                    handleFieldChange(field.label, e.target.value)
+                  }
+                />
+              )}
             </div>
-        )
+          ))}
+
+          <button className="modal-save" onClick={handleFormSave}>
+            Save Changes
+          </button>
+        </div>
+      </div>
     )
-}
+  );
+};
 
 export default Modal;
