29a0e43f7620cc0801000c4780d67b69d4adcfcb
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-04 20:08:43 +0100

    Update Config.js

--- basic-crud/src/components/Config.js
+++ basic-crud/src/components/Config.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Config = () => {
-    return (
-        <div>
-            <h1>Config</h1>
-            <p>Coming soon...</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Config</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+};
 
 export default Config;
