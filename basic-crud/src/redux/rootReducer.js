44043c8b70d37022b9d5ac02afb21882ac5622b2
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-04 20:10:40 +0100

    Update rootReducer.js

--- basic-crud/src/redux/rootReducer.js
+++ basic-crud/src/redux/rootReducer.js
@@ -1,12 +1,12 @@
-import { combineReducers  } from "redux";
+import { combineReducers } from "redux";
 import ordersReducer from "./ordersSlice";
 import refundsReducer from "./refundsSlice";
 import customerReducer from "./customersSlice";
 
 const rootReducer = combineReducers({
-    orders: ordersReducer,
-    refunds: refundsReducer,
-    customers: customerReducer,
+  orders: ordersReducer,
+  refunds: refundsReducer,
+  customers: customerReducer,
 });
 
 export default rootReducer;
