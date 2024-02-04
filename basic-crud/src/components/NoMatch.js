e5d81586b140d7a2c2fa5865fc8f1e9cc3f8a9c0
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-04 20:09:45 +0100

    Update NoMatch.js

--- basic-crud/src/components/NoMatch.js
+++ basic-crud/src/components/NoMatch.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const NoMatch = () => {
-    return (
-        <div>
-            <h1>404: Page Not Found</h1>
+  return (
+    <div>
+      <h1>404: Page Not Found</h1>
 
-            <p>Sorry, the page you are looking for does not exist.</p>
-        </div>
-    )
-}
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
 
 export default NoMatch;
