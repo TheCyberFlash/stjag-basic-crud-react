6afafe1140bc78f53702deaf004a7153d0a0247b
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-04 20:10:07 +0100

    Update Sidebar.js

--- basic-crud/src/components/Sidebar.js
+++ basic-crud/src/components/Sidebar.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faShoppingCart, faUsers, faUndo, faCog } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHome,
+  faShoppingCart,
+  faUsers,
+  faUndo,
+  faCog,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <Link to="/"><FontAwesomeIcon icon={faHome} />{' '} Home</Link>
-            <Link to="/orders"><FontAwesomeIcon icon={faShoppingCart} />{' '} Orders</Link>
-            <Link to="/customers"><FontAwesomeIcon icon={faUsers} />{' '} Customers</Link>
-            <Link to="/refunds"><FontAwesomeIcon icon={faUndo} />{' '} Refunds</Link>
-            <Link to="/config"><FontAwesomeIcon icon={faCog} />{' '} Config</Link>
-        </div>
-    )
-}
+  return (
+    <div className="sidebar">
+      <Link to="/">
+        <FontAwesomeIcon icon={faHome} /> Home
+      </Link>
+      <Link to="/orders">
+        <FontAwesomeIcon icon={faShoppingCart} /> Orders
+      </Link>
+      <Link to="/customers">
+        <FontAwesomeIcon icon={faUsers} /> Customers
+      </Link>
+      <Link to="/refunds">
+        <FontAwesomeIcon icon={faUndo} /> Refunds
+      </Link>
+      <Link to="/config">
+        <FontAwesomeIcon icon={faCog} /> Config
+      </Link>
+    </div>
+  );
+};
 
 export default Sidebar;
