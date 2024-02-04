a0abff042bd7c197202cca55e5a575333ad2dfcf
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-04 20:09:52 +0100

    Update Orders.js

--- basic-crud/src/components/Orders.js
+++ basic-crud/src/components/Orders.js
@@ -2,56 +2,82 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import DataTable from "./DataTable";
 import Modal from "./Modal";
-import { selectFiltersData, addOrder, updateFilters } from "../redux/ordersSlice";
+import {
+  selectFiltersData,
+  addOrder,
+  updateFilters,
+} from "../redux/ordersSlice";
 
 const Orders = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [editedRecord, setEditedRecord] = useState(null);
-    const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editedRecord, setEditedRecord] = useState(null);
+  const dispatch = useDispatch();
 
-    const { filters, filterDisplayNames, fields, columns } = useSelector((state) => state.orders);
-    const filteredData = useSelector(selectFiltersData);
+  const { filters, filterDisplayNames, fields, columns } = useSelector(
+    (state) => state.orders
+  );
+  const filteredData = useSelector(selectFiltersData);
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    }
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    }
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    const handleCreate = () => {
-        openModal();
-        setEditedRecord(null);
-    }
+  const handleCreate = () => {
+    openModal();
+    setEditedRecord(null);
+  };
 
-    const handleEdit = (record) => {
-        openModal();
-        setEditedRecord(record);
-    }
+  const handleEdit = (record) => {
+    openModal();
+    setEditedRecord(record);
+  };
 
-    const handleDelete = (record) => {
-        alert(`Delete action for row with Order No.: ${record['Order No.']}`);
-    }
+  const handleDelete = (record) => {
+    alert(`Delete action for row with Order No.: ${record["Order No."]}`);
+  };
 
-    const handleSaveChanges = (formValues) => {
-        dispatch(addOrder(formValues));
-        closeModal();
-    }   
+  const handleSaveChanges = (formValues) => {
+    dispatch(addOrder(formValues));
+    closeModal();
+  };
 
-    const handleFilterChange = (newFilters) => {
-        const { delStatus, payProtect, refundEligible } = newFilters;
-        dispatch(updateFilters({ delStatusFilter: delStatus, payProtectFilter: payProtect, refundEligibleFilter: refundEligible }));
-    }    
-    
-    return (
-        <div>
-            <h1>Orders</h1>
-            <DataTable columns={columns} data={filteredData} handleCreate={handleCreate} handleEdit={handleEdit} 
-                handleDelete={handleDelete} filters={filters} filterDisplayNames={filterDisplayNames} handleFilterChange={handleFilterChange}/>
-            <Modal isOpen={isModalOpen} closeModal={closeModal} fields={fields} handleSaveChanges={handleSaveChanges} editedRecord={editedRecord} />
-        </div>
-    )
-}
+  const handleFilterChange = (newFilters) => {
+    const { delStatus, payProtect, refundEligible } = newFilters;
+    dispatch(
+      updateFilters({
+        delStatusFilter: delStatus,
+        payProtectFilter: payProtect,
+        refundEligibleFilter: refundEligible,
+      })
+    );
+  };
+
+  return (
+    <div>
+      <h1>Orders</h1>
+      <DataTable
+        columns={columns}
+        data={filteredData}
+        handleCreate={handleCreate}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        filters={filters}
+        filterDisplayNames={filterDisplayNames}
+        handleFilterChange={handleFilterChange}
+      />
+      <Modal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        fields={fields}
+        handleSaveChanges={handleSaveChanges}
+        editedRecord={editedRecord}
+      />
+    </div>
+  );
+};
 
 export default Orders;
