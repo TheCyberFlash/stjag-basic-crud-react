bee1f27b7e280ac676fd2f15e4b9822ecebca046
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-04 20:09:24 +0100

    Update Filter.js

--- basic-crud/src/components/Filter.js
+++ basic-crud/src/components/Filter.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 const Filter = ({ label, value, options, onChange }) => {
-    return (
-        <div className="filter-container">
-            <label>{label}</label>
-            <select value={value.toString()} onChange={(e) => onChange(e.target.value)}>
-                {options.map((option, index) => (
-                    <option key={index} value={option.value.toString()}>
-                        {option.display}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
-}
+  return (
+    <div className="filter-container">
+      <label>{label}</label>
+      <select
+        value={value.toString()}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        {options.map((option, index) => (
+          <option key={index} value={option.value.toString()}>
+            {option.display}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default Filter;
